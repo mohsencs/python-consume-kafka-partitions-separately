74b6b7c7e1a01144f488b48e3718b005de61a857
mohsencs <mohsensoodkhah@gmail.com>
2020-05-19 15:30:02 +0430

    Create consume_partition_1.py

---
+++ consume_partition_1.py
@@ -0,0 +1,15 @@
+from kafka import TopicPartition
+from kafka import KafkaConsumer
+
+topic = 'test1'
+partition = 1
+tp = TopicPartition(topic,partition)
+server_addresses = ['192.168.0.5:9092']
+
+
+kafkaConsumer = KafkaConsumer(group_id="a", auto_offset_reset='earliest', 
+        bootstrap_servers=server_addresses, enable_auto_commit=True, auto_commit_interval_ms=1000)
+kafkaConsumer.assign([tp])
+
+for msg in kafkaConsumer:
+    print(msg.value)
